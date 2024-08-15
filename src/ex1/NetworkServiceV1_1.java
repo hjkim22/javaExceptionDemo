60f41b3f883fe1a345697bc726ef96b8dbf14f81
hjkim <hjhjgudwls1022@gmail.com>
2024-08-15 19:45:13 +0900

    오류 상황 만들기

---
+++ src/ex1/NetworkServiceV1_1.java
@@ -0,0 +1,14 @@
+package ex1;
+
+public class NetworkServiceV1_1 {
+
+    public void sendMessage(String data) {
+        String address = "http://example.com";
+        NetworkClientV1 client = new NetworkClientV1(address);
+        client.ininError(data);
+
+        client.connect();
+        client.send(data);
+        client.disconnect();
+    }
+}
